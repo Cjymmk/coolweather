f8ad6d277769974ce107247f7f12c7b4e6927864
Cjymmk <928574519@qq.com>
2017-05-08 09:59:13 +0800

    2

--- app/src/main/java/com/example/ruili/coolweather/MainActivity.java
+++ app/src/main/java/com/example/ruili/coolweather/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.ruili.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+  private String[]data={"北京","上海","浙江","天津","重庆","香港","澳门","台湾","黑龙江","吉林",
+  };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+             MainActivity.this, android.R.layout.simple_list_item_1,data);
+        ListView listView=(ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
