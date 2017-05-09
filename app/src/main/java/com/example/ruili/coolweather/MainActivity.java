e0a0c2505ca21a8c94282800246c35445ba4386f
Cjymmk <928574519@qq.com>
2017-05-09 10:34:36 +0800

    3

--- app/src/main/java/com/example/ruili/coolweather/MainActivity.java
+++ app/src/main/java/com/example/ruili/coolweather/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.ruili.coolweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
   private String[]data={"北京","上海","浙江","天津","重庆","香港","澳门","台湾","黑龙江","吉林",
@@ -17,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
              MainActivity.this, android.R.layout.simple_list_item_1,data);
         ListView listView=(ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?>parent, View view,int position,long id){
+                Toast.makeText(MainActivity.this,data[position],Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,City.class);
+                intent.putExtra("p",data[position]);
+               startActivity(intent);
+            }
+        });
     }
 }
