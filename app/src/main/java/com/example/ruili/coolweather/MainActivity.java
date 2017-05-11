591f7ee3db0c405e5f878e9e9e180e46a123c0eb
Cjymmk <928574519@qq.com>
2017-05-11 10:49:56 +0800

    5

--- app/src/main/java/com/example/ruili/coolweather/MainActivity.java
+++ app/src/main/java/com/example/ruili/coolweather/MainActivity.java
@@ -11,25 +11,25 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-  private String[]data={"北京","上海","浙江","天津","重庆","香港","澳门","台湾","黑龙江","吉林",
-  };
-    private String[]citydata1={"北京市","浙江省"};
-    private String[]citydata2={"杭州、湖州、嘉兴、宁波","温州","丽水","金华","舟山","衢州","台州",};
+
+    private String[]citydata1={"北京","浙江省"};
+    private String[]citydata2={"杭州","湖州","嘉兴","宁波","温州","丽水","金华","舟山","衢州","台州",};
     private  String[]getCitydata3={"北京"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1=(Button) findViewById(R.id.button);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,data);
+        Button btn1=(Button) findViewById(R.id.button2);
+        btn1.setVisibility(View.VISIBLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,citydata1);
         final ListView listView=(ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?>parent, View view,int position,long id){
-                Toast.makeText(MainActivity.this,data[position],Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,citydata1[position],Toast.LENGTH_SHORT).show();
                 String[] data=null;
-                if("北京".equals(data[position])){
+                if("北京".equals(citydata1[position])){
                     data=getCitydata3;
                 }
                 else
